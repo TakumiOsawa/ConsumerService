e60eb47d82e672baf469706c10bde5a119ff5dd6
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-10 19:46:54 +0900

    Initial commit

---
+++ src/test/java/com/ftgo/ConsumerService/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftgo.ConsumerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
